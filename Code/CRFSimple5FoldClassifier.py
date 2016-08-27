c90c81c959f5b8804311f087a0914f21eeac7782
MLDroid <annamalai.nr@gmail.com>
2016-08-27 13:30:30 +0800

    CRF code without CV DONE!!!! Solved a 2 day-long problem!!!

--- Code/CRFSimple5FoldClassifier.py
+++ Code/CRFSimple5FoldClassifier.py
@@ -35,7 +35,6 @@ def TrainTestSplit (x, y, test_size = 0.2,random_state=42):
     y_test = y[CutOff:]
     return x_train, x_test, y_train, y_test
 
-
 def GetYFromStringLabels (Labels):
     Y = []
     for L in Labels:
@@ -54,61 +53,45 @@ def GetYFromStringLabels (Labels):
 
 def GetListOfDict (X):
     X_OP = []
-    ListOfDicts = []
     for Sents in X:
+        ListOfDict = []
         SentAsDict = OrderedDict()
         for Index, SentFeat in enumerate(Sents):
-            SentAsDict[str(Index)] = SentFeat
-        ListOfDicts.append(SentAsDict)
-    X_OP.append(ListOfDicts)
+            SentAsDict[str(Index)] = str(SentFeat)
+        ListOfDict.append(SentAsDict)
+        X_OP.append(ListOfDict)
+    # print len(X_OP)
+    # print len(X_OP[0])
+    # print X_OP[0]
+    # raw_input()
     return X_OP
 
-Samples = [l.strip().split() for l in open ('../Data/JUSCEFeats.txt').xreadlines()]
-Labels = [[l.strip().split(';')[-1]] for l in open ('../Data/RestAspCatABSA.csv')]
-# Y = GetYFromStringLabels(Labels)
-Y = Labels
-print 'loaded {} samples and {} labels'.format(len(Samples), len (Y))
+X = [l.strip().split() for l in open ('../Data/JUSCEFeats.txt').xreadlines()]
+Y = [[str(l.strip().split(';')[-1])] for l in open ('../Data/RestAspCatABSA.csv')]
+print 'loaded {} samples and {} labels'.format(len(X), len (Y))
+X_train, X_test, y_train, y_test = TrainTestSplit (X, Y, test_size = 0.2,random_state=randint(0,100))
+print 'train and test shapes', len(X_train), len(X_test), len(y_train), len(y_test)
+X_train = GetListOfDict(X=X_train)
+X_test = GetListOfDict(X=X_test)
+# print '****'
+print X_test[0]
+print y_test[0]
+# raw_input()
+crf = sklearn_crfsuite.CRF(
+    algorithm='lbfgs',
+    c1=0.1,
+    c2=0.1,
+    max_iterations=100,
+    all_possible_transitions=True
+)
+crf.fit(X_train, y_train)
 
-Accs = [];Ps = [];Rs = [];Fs = []
-for i in xrange (5):
-    print 'run ',i+1
-    X_train, X_test, y_train, y_test = TrainTestSplit (Samples, Y, test_size = 0.2,random_state=randint(0,100))
-    print 'train and test shapes', len(X_train), len(X_test), len(y_train), len(y_test)
-    X_train = GetListOfDict(X=X_train)
-    X_test = GetListOfDict(X=X_test)
-    Tmp1 = [];Tmp1.append(y_train);y_train = Tmp1
-    Tmp2 = [];Tmp2.append(y_test);y_test = Tmp2
-    print X_train[0][0], y_train[0][0]
-    # print X_test[0], y_test[0]
-    raw_input()
-    crf = sklearn_crfsuite.CRF(
-        algorithm='lbfgs',
-        c1=0.1,
-        c2=0.1,
-        max_iterations=100,
-        all_possible_transitions=True
-    )
-    crf.fit(X_train, y_train)
+Preds = crf.predict(X_test)
+Acc = accuracy_score(y_true = y_test, y_pred=Preds)
+P = precision_score(y_true = y_test, y_pred=Preds)
+R = recall_score(y_true = y_test, y_pred=Preds)
+F = f1_score(y_true = y_test, y_pred=Preds)
+print (Acc,P,R,F)
 
-    Preds = y_pred = crf.predict(X_test)
-    Acc = accuracy_score(y_true = y_test, y_pred=Preds)
-    P = precision_score(y_true = y_test, y_pred=Preds)
-    R = recall_score(y_true = y_test, y_pred=Preds)
-    F = f1_score(y_true = y_test, y_pred=Preds)
-    Accs.append(Acc); Ps.append(P);Rs.append(R);Fs.append(F)
-    print (Acc,P,R,F)
-    print classification_report (y_test, Preds)
-
-Accs = np.array (Accs)
-Ps = np.array (Ps)
-Rs = np.array (Rs)
-Fs = np.array (Fs)
-MeanA = np.mean(Accs); StdA = np.std(Accs)
-MeanP = np.mean(Ps); StdP = np.std(Ps)
-MeanR = np.mean(Rs); StdR = np.std(Rs)
-MeanF = np.mean(Fs); StdF = np.std(Fs)
-
-print 'Average Acc: {}, Prec: {}, Recall: {}, F1: {}'.format(MeanA, MeanP, MeanR, MeanF)
-print 'Std Acc: {}, Prec: {}, Recall: {}, F1: {}'.format(StdA, StdP, StdR, StdF)
 
 
