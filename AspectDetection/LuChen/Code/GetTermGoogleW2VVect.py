08bf1879520eedb8f2c9cf44edcf91f545592ec7
MLDroid <annamalai.nr@gmail.com>
2016-09-21 04:32:24 +0800

    imp bug dix: code to get asp terms that are in google w2v file (remove others)

--- AspectDetection/LuChen/Code/GetTermGoogleW2VVect.py
+++ AspectDetection/LuChen/Code/GetTermGoogleW2VVect.py
@@ -14,7 +14,7 @@ def LoadGoogleNewsW2VModel (ModelFName):
     return W2VModel
 
 
-FName = '/home/annamalai/Desktop/ABSA/AspectDetection/LuChen/DataAndGT/reviews_TV_AspTerms.txt'
+FName = '/home/annamalai/Desktop/ABSA/AspectDetection/LuChen/DataAndGT/reviews_GPS_AspTerms.txt'
 # FName = sys.argv[1]
 W2VModelFName = '/home/annamalai/Desktop/ABSA/Embeddings/GoolgeNews/GoogleNews-vectors-negative300.bin'
 
@@ -24,10 +24,12 @@ D = OrderedDict()
 W2VModel = LoadGoogleNewsW2VModel(W2VModelFName)
 #to load syn0norm
 W2VModel.most_similar('test','testing')
+TermsToDel = []
 for T in Terms:
     try:
         TIndex = W2VModel.vocab[T].index
     except:
+        TermsToDel.append(T)
         continue
     Vec = W2VModel.syn0norm[TIndex]
     D[T] = Vec.tolist()
@@ -38,8 +40,17 @@ with open (OpFName,'w') as FH:
 
 print 'check file ', OpFName
 
-
-
+print '-'*80
+print 'terms to be deleted from ',FName
+pprint(TermsToDel)
+print '-'*80
+print 'after deleting'
+print '-'*80
+TermsToDel = set (TermsToDel)
+for T in Terms:
+    if T not in TermsToDel:
+        print T
+print '-'*80
 
 
 
