839bf4d042332f2c14091d0d4144c6ccbd9b5988
MLDroid <annamalai.nr@gmail.com>
2016-09-24 15:36:47 +0800

    some clean up

--- AspectDetection/LuChen/Code/WorndetSim.py
+++ AspectDetection/LuChen/Code/WorndetSim.py
@@ -12,30 +12,24 @@ def wordnet_sim(s1, s2, index1=0, index2=0):
     if s1 == s2:
         print 'same term found: ', s1, s2
         return 1.0
-    # try:
-    t0 = time()
-    wordnet_s1 = wordnet.synsets(s1)[0]
-    wordnet_s2 = wordnet.synsets(s2)[0]
-    sim = wordnet_s1.wup_similarity(wordnet_s2)
-    if not sim:
-        sim = 0.0
-    # print sim
-    # raw_input()
-    print 'completed similarty for {} and {} (i.e., {} and {}) to be {} in {} sec'.format(s1,s2,
-                                                                                          wordnet_s1, wordnet_s2,
-                                                                                          round(sim,2),round(time()-t0,2))
-    return sim
-    # except:
-    #     print 'Error in getting similarity for {}: {}'.format(s1,s2)
-    #     return -1.0
+    try:
+        t0 = time()
+        wordnet_s1 = wordnet.synsets(s1)[0]
+        wordnet_s2 = wordnet.synsets(s2)[0]
+        sim = wordnet_s1.wup_similarity(wordnet_s2)
+        if not sim:
+            sim = 0.0
+        # print sim
+        # raw_input()
+        print 'completed similarty for {} and {} (i.e., {} and {}) to be {} in {} sec'.format(s1,s2,
+                                                                                              wordnet_s1, wordnet_s2,
+                                                                                              round(sim,2),round(time()-t0,2))
+        return sim
+    except:
+        print 'Error in getting similarity for {}: {}'.format(s1,s2)
+        return -1.0
 
-def GetSimWithAllTerms (TgtTerm, TgtTermIndex, AllTerms):
-    # Sims = [sss(TgtTerm, Term) for Term in AllTerms]
-    Sims = Parallel(n_jobs=12)(delayed(wordnet_sim)(TgtTerm, Term, index1=TgtTermIndex, index2=Index)
-                               for Index, Term in enumerate(AllTerms))
-    return Sims
 
-# AspTermsFName = '/mnt/AnnaLaptop/Desktop/ABSA/AspectDetection/LuChen/DataAndGT/reviews_TV_AspTermsAfterRemNonGooglew2v.txt'
 AspTermsFName = '/home/annamalai/Desktop/ABSA/AspectDetection/LuChen/DataAndGT/reviews_TV_AspTermsAfterRemNonGooglew2v.txt'
 AspTerms = [l.strip() for l in open(AspTermsFName).xreadlines()][:10]
 print 'loaded {} asp terms from {}'.format(len(AspTerms), AspTermsFName)
@@ -47,18 +41,13 @@ for RTermIndex, RTerm in enumerate(AspTerms):
             Sim = wordnet_sim(RTerm, CTerm, RTermIndex, CTermIndex)
             WordnetSim[RTermIndex, CTermIndex] = Sim
 
-#
-# WordnetSim = []
-# for Index, Term in enumerate(AspTerms):
-#     Sims = GetSimWithAllTerms(Term, Index, AspTerms)
-#     WordnetSim.append(Sims)
 
-# OpFName = AspTermsFName.replace('AspTermsAfterRemNonGooglew2v.txt','WordnetSimList.txt')
-# with open (OpFName,'w') as FH:
-#     for Row in WordnetSim:
-#         print>>FH, (' '.join([str(S) for S in Row]))
-#
 WordnetSim = np.array (WordnetSim)
+for RTermIndex in xrange(len(AspTerms)):
+    for CTermIndex in xrange(len(AspTerms)):
+        if RTermIndex < CTermIndex:
+            WordnetSim[RTermIndex, CTermIndex] = WordnetSim[CTermIndex, RTermIndex]
+
 print 'obtained wordnet SimMat of shape', WordnetSim.shape
 OpFName = AspTermsFName.replace('AspTermsAfterRemNonGooglew2v.txt','WordnetSim.txt')
 np.savetxt(OpFName, WordnetSim, fmt='%.4f')
